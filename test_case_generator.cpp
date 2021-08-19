d8bf2fb2cf8eae19b062bb46824ab3a51faebfe7
Pratyush Gupta <82253040+PratyushGupta0@users.noreply.github.com>
2021-08-19 16:54:56 +0530

    Update test_case_generator.cpp

--- test_case_generator.cpp
+++ test_case_generator.cpp
@@ -3,38 +3,39 @@ using namespace std;
 
 long int find_number(vector<long int>& v)
 {
-long int vec_length = v.size();
-srand(time(NULL));
-long int index = rand() % n=vec_length;
-long int num = v[index];
-swap(v[index], v[vec_length - 1]);
-v.pop_back();
-return num;
+    srand(time(NULL));
+    long int index=(rand()) % v.size();
+    long int num = v[index];
+    long int x=v[index];
+    v[index]=x;
+    v[vec_length-1]=x;
+    v.pop_back();
+    return num;
 }
 
 vector<long int> get_random(long int n)
 {
-vector<long int> v(n);
-for (long int i = 0; i < n; i++)
-v[i] = i + 1;
-return v;
+    vector<long int> v(n);
+    for (long int i = 0; i < n; i++){
+       v[i] = i + 1;}
+    return v;
 
 }
 long int single_random(long int upper_bound)
 {
-return (rand() %(upper_bound - 1000 + 1)) + 1000;
-}// Driver code
+    return (rand() %(upper_bound - 100 + 1)) + 100;
+}
 int main()
 {
-int testcases=50000;
-vector<vector<long int>> final;
-for(int i=0;i<testcases)
-{
-srand(time(0));
-long int n=single_random(1e10);
-cout<<n<<endl;
-final.push_back(get_random(n));
-}
+    int testcases=50000;
+    vector<vector<long int>> final;
+    for(int i=0;i<testcases;++i)
+    {
+        srand(time(0));
+        long int n=single_random(1e10);
+        cout<<n<<endl;
+        final.push_back(get_random(n));
+    }
 
-return 0;
+    return 0;
 }
